2b54ac7c4f02828aa7320cbc6b3bb02d3bed1fa4
Pankaj Jaiswal <pankajj112223333@gmail.com>
2024-01-17 15:25:12 +0530

    added allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cyupn96w2e&1#mpij7f6k$r9qq4590t9#a$zuedeaj8$7%sk%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
